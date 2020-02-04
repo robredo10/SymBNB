2853fb4843949361913b79ae74da86b6f1e0ae43
unknown <you@example.com>
2020-02-04 14:23:34 +0100

    Premier commit, installation et paged'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/bonjour/{prenom}/age/{age}", name="hello")
+     * @Route("/salut", name="hello_base")
+     * @Route("/bonjour/{prenom}", name="hello_prenom")
+     * Montre la page qui dit bonjour
+     * @return void
+     */
+    public function hello($prenom = "anonyme", $age = 0) {
+        
+        return $this->render('hello.html.twig',
+            [
+                'prenom' => $prenom,
+                'age' => $age
+            ]
+        );
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        $prenom = ["Lior" => 31, "Joseph" => 12, "Anne" => 55];
+
+        return $this->render(
+            'home/home.html.twig',
+            [
+                'title' => "Bonjour à tous",
+                'age' => 12,
+                'tableau' => $prenom
+            ]
+        );
+    }
+}
